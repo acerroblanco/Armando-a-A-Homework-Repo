af8c5b4648e1f08ea1b7f4eed378472c63396236
acerroblanco <acerroblanco@gmail.com>
2018-10-30 08:54:56 -0700

    W2D1 homework

---
+++ W2D1/Armando_Cerroblanco_Macala_skeleton 2/lib/board.rb
@@ -0,0 +1,69 @@
+class Board
+  attr_accessor :cups
+
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups =  Array.new(14).map.with_index do |pos, i|
+      if i == 6 || i == 13
+        []
+      else
+        [:stone, :stone, :stone, :stone]
+      end
+    end
+  end
+
+
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+    # self.cups.map.with_index do |pos, i|
+    #   if i == 6 || i == 13
+    #     []
+    #   else
+    #     [:stone, :stone, :stone, :stone]
+    #   end
+    # end
+  end
+
+  def valid_move?(start_pos)
+
+    if start_pos > 12
+      raise 'Invalid starting cup'
+    elsif @cups[start_pos-1].empty?
+      raise 'Starting cup is empty'
+    end
+  end
+
+  def make_move(start_pos, current_player_name)
+    @cups[start_pos] = []
+    @cups[start_pos+1..start_pos+4].map do |cup|
+      cup << :stone
+    end
+
+    # self.render
+    # self.next_turn()
+
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+
+  end
+
+  def winner
+
+  end
+end
